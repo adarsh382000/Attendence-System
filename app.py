8f1dd591830e042bdfce19bb61984c959c458160
Adarsh Goswami <72919333+adarsh382000@users.noreply.github.com>
2021-07-03 23:37:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,8 @@ def main():
             if str(pas) == str(password):
                 st.write("Upload the Image of Person to Register them in DataBase")
                 uploaded_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
-                
+                if uploaded_file is None:
+                    st.write("none")
                 if uploaded_file is not None:
                     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
                     image = cv2.imdecode(file_bytes, 1)
