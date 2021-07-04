6cd08fe89f8aeec6e46e2faad94f0c3487363119
Adarsh Goswami <72919333+adarsh382000@users.noreply.github.com>
2021-07-04 10:27:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def main():
   if choice == "Mark Attendence":
     st.write("Please use image with frontal angle and image should be well lit")
     st.write("Example image:")
-    st.image(image.jpeg)
+    st.image(image.jpg)
     uploaded_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
     
     if uploaded_file is not None:
