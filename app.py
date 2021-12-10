1b834ea06b113e8795cb43a24670432e7a2c4b4a
Adarsh Goswami <72919333+adarsh382000@users.noreply.github.com>
2021-12-10 19:18:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ train_model = st.secrets["train_model"]
 def face_recognition_model():
     try:
         gdown.download(train_model, 'model.h5', quiet=False)
-        model = keras.models.load_model('model.h5')
+        model = keras.models.load_model('model.h5',compile=False)
         return model
     except Exception:
         st.write("Error loading predictive model")
